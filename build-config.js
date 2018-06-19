2782cde28aa73d99139848d0ccd5ba28a24d9006
Matthew Dobson <mjadobson@gmail.com>
2018-06-19 23:04:51 +0100

    init

--- build-config.js
+++ build-config.js
@@ -10,50 +10,41 @@ module.exports = {
 	editor: '@ckeditor/ckeditor5-editor-classic/src/classiceditor',
 
 	// The name under which the editor will be exported.
-	moduleName: 'ClassicEditor',
+	moduleName: 'VetReviseEditor',
 
 	// Plugins to include in the build.
 	plugins: [
 		'@ckeditor/ckeditor5-essentials/src/essentials',
-
-		// '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter',
 		'@ckeditor/ckeditor5-autoformat/src/autoformat',
 		'@ckeditor/ckeditor5-basic-styles/src/bold',
 		'@ckeditor/ckeditor5-basic-styles/src/italic',
-		'@ckeditor/ckeditor5-block-quote/src/blockquote',
-		// '@ckeditor/ckeditor5-easy-image/src/easyimage',
-		'@ckeditor/ckeditor5-heading/src/heading',
+		'@ckeditor/ckeditor5-basic-styles/src/underline',
+		'ckeditor5-sss-upload/src/s3upload',
+		'ckeditor5-super-sub/index',
 		'@ckeditor/ckeditor5-image/src/image',
-		'@ckeditor/ckeditor5-image/src/imagecaption',
-		'@ckeditor/ckeditor5-image/src/imagestyle',
-		'@ckeditor/ckeditor5-image/src/imagetoolbar',
 		'@ckeditor/ckeditor5-image/src/imageupload',
 		'@ckeditor/ckeditor5-link/src/link',
 		'@ckeditor/ckeditor5-list/src/list',
-		'@ckeditor/ckeditor5-paragraph/src/paragraph',
-		'ckeditor5-s3-upload/src/s3upload'
+		'@ckeditor/ckeditor5-paragraph/src/paragraph'
 	],
 
 	// Editor config.
 	config: {
 		toolbar: {
 			items: [
-				'heading',
-				'|',
 				'bold',
 				'italic',
+				'underline',
+				'super',
+				'sub',
 				'link',
 				'bulletedList',
 				'numberedList',
 				'imageUpload',
-				'blockQuote',
+				'|',
 				'undo',
 				'redo'
 			]
-		},
-
-		image: {
-			toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
 		},
 
 		// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -3,65 +3,52 @@
  * For licensing, see LICENSE.md.
  */
 
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import VetReviseEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
 import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockquotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
+import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import S3uploadPlugin from 'ckeditor5-sss-upload/src/s3upload';
+import IndexPlugin from 'ckeditor5-super-sub/index';
 import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
-import ImagecaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImagestylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImagetoolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageuploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload';
 import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
 import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
 import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import S3uploadPlugin from 'ckeditor5-s3-upload/src/s3upload';
 
-export default class ClassicEditor extends ClassicEditorBase {}
+export default class VetReviseEditor extends VetReviseEditorBase {}
 
-ClassicEditor.build = {
+VetReviseEditor.build = {
 	plugins: [
 		EssentialsPlugin,
 		AutoformatPlugin,
 		BoldPlugin,
 		ItalicPlugin,
-		BlockquotePlugin,
-		HeadingPlugin,
+		UnderlinePlugin,
+		S3uploadPlugin,
+		IndexPlugin,
 		ImagePlugin,
-		ImagecaptionPlugin,
-		ImagestylePlugin,
-		ImagetoolbarPlugin,
 		ImageuploadPlugin,
 		LinkPlugin,
 		ListPlugin,
-		ParagraphPlugin,
-		S3uploadPlugin
+		ParagraphPlugin
 	],
 	config: {
 		toolbar: {
 			items: [
-				'heading',
-				'|',
 				'bold',
 				'italic',
+				'underline',
+				'super',
+				'sub',
 				'link',
 				'bulletedList',
 				'numberedList',
 				'imageUpload',
-				'blockQuote',
+				'|',
 				'undo',
 				'redo'
-			]
-		},
-		image: {
-			toolbar: [
-				'imageStyle:full',
-				'imageStyle:side',
-				'|',
-				'imageTextAlternative'
 			]
 		},
 		language: 'en'
